2bf5a78171370102f0d18620d373f2b23eca68f5
PeredaG <70949726+peredag@users.noreply.github.com>
2023-03-16 12:55:29 -0600

    M10 (#30)
    * controlador y ruta de busqueda de mangas por titulo y por categoria
    
    * corrijo controlador
    
    * corrijo metodo get_mangas
    
    * pruebas controlador para consulta por query category_id
    
    * cambios controlador manga, consultas query por title y por categoria … (#25)
    
    * cambios controlador manga, consultas query por title y por categoria funcionando
    
    * arreglitos
    
    * mangas controller

--- models/Manga.js
+++ models/Manga.js
@@ -3,23 +3,17 @@ import mongoose from "mongoose";
 
 let schema = new mongoose.Schema(
   {
+    author_id: { type: mongoose.Types.ObjectId, ref: 'authors', required: true },
+    company_id: { type: mongoose.Types.ObjectId, ref: 'companies' },
     title: { type: String, required: true },
-    description:{ type: String, required: true },
-    category: {type: String, required: true}
-  },
-  {
-    timestamps: true,
+    cover_photo: { type: String, required: true },
+    description: { type: String, required: true },
+    category_id: { type: mongoose.Types.ObjectId, ref: 'categories', required: true }
+  },{
+    timestamps: true
   }
+
 );
-
 let Manga = mongoose.model("mangas", schema);
-
 export default Manga;
 
-/* 
-
-
-"title": "xxxx",
-"description": "xxxxx",
-"category": "xxxx"
-*/
