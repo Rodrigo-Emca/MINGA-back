ed6cc98344c0504b693853e3e20143dc10899c3e
Alexis Franco <108986498+Alexis196@users.noreply.github.com>
2023-03-16 18:21:14 -0300

    correccion ruta auth/users (#32)

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import controller from '../controllers/auth.js';
+import controller from '../controllers/auth/auth.js'
 import postSchemaSignUp from '../schemas/usersSignUp.js'
 import validator from '../middlewares/validator.js';
 import accountExistsSignUp from '../middlewares/auth/accountExistsSignUp.js'
@@ -12,12 +12,11 @@ import passport from '../middlewares/auth/passport.js'
 
 let router = express.Router()
 
-const {sign_up, sign_in, sign_out, signintoken} = controller
+const {sign_up, sign_in, sign_out, token } = controller
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   return res
-    .send('Aqui deberían aparecer todos los usuarios.')
     .send('Aqui deberían aparecer todos los usuarios.')
     .status(200)
 });
@@ -42,24 +41,6 @@ router.post('/signout',
   passport.authenticate('jwt',{session:false}),
   sign_out
 //POST auths listing:
-)
-router.post('/signup', 
-  validator(postSchemaSignUp),
-  accountExistsSignUp,
-  sign_up
-)
-
-router.post('/signin', 
-  validator(postSchemaSignIn),
-  accountExistsSignIn,
-  accountHasBeenVerified,
-  passwordIsOk,
-  sign_in
-)
-
-router.post('/signout',
-  passport.authenticate('jwt',{session:false}),
-  sign_out
 )
 
 router.post('/token', passport.authenticate('jwt',{session:false}),token)
