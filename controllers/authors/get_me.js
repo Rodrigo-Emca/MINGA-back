5b493275cc8f1b560436816613d4230d55b8fa3a
PeredaG <70949726+peredag@users.noreply.github.com>
2023-03-29 19:55:37 -0600

    M19 dev pull (#49)
    * controlador y ruta de busqueda de mangas por titulo y por categoria
    
    * corrijo controlador
    
    * corrijo metodo get_mangas
    
    * pruebas controlador para consulta por query category_id
    
    * cambios controlador manga, consultas query por title y por categoria funcionando
    
    * arreglitos
    
    * mangas controller
    
    * prueba
    
    * controladores y rutas, get author, update author
    
    * controllador y rutas para authors y update
    
    * algunas correcciones
    
    * correcciones
    
    * pruebas
    
    * agrego authors
    
    * m19
    
    * correcciones en creación de authores
    
    * m19
    
    * cambios

--- controllers/authors/get_me.js
+++ controllers/authors/get_me.js
@@ -20,6 +20,23 @@ const controller = {
             });
         }
     },
+    getMe:async(req,res,next)=>{
+        try{
+            let author = await Author.findOne({user_id: req.user})
+            if(author){
+                return res.status(200).json({
+                    success: true,
+                    me,
+                });
+            }
+            return next(createError(404, "Author not found"))
+        } catch (error) {
+            return res.status(400).json({
+                success: false,
+                message: "Server error",
+            });
+        }
+    },
 };
 
 export default controller
